0e54db9b80f805aa2f39a1cf02a99f08824584d5
Vitaly <onebeattrue@gmail.com>
2023-09-03 23:48:00 +0300

    Создал класс RefreshDataWorkerFactory, переопределил конфигурацию создания Worker'ов, передал нужные параметры в конструктор

--- app/src/main/java/com/example/cryptoapp/presentation/CoinApplication.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApplication.kt
@@ -1,10 +1,27 @@
 package com.example.cryptoapp.presentation
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
 
-class CoinApplication : Application() {
+class CoinApplication : Application(), Configuration.Provider {
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(
+                RefreshDataWorkerFactory(
+                    coinInfoDao = AppDatabase.getInstance(this).coinPriceInfoDao(),
+                    coinMapper = CoinMapper(),
+                    apiService = ApiFactory.apiService
+                )
+            )
+            .build()
     }
 }
